6f2d238a26f15ab3dff1ee337a7e28969c28dd51
Mr Sagar <137499419+sbplays10@users.noreply.github.com>
2023-10-03 12:58:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,7 +96,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', '')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 
 #Openai
-AI = is_enabled((environ.get("AI","True")), True)
+AI = is_enabled((environ.get("AI","True")), False)
 OPENAI_API = environ.get("OPENAI_API"," ")
 AI_LOGS = int(environ.get("AI_LOGS"," ")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
 
